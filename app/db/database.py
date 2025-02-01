11075b99cf9c32ae27ecf4b37ebce53fc0797e08
oasilturk <moasilturk@gmail.com>
2025-02-01 23:40:58 +0300

    fixup database config file

--- app/db/database.py
+++ app/db/database.py
@@ -1,7 +1,10 @@
-from motor.motor_asyncio import AsyncIOMotorClient
 import os
+from motor.motor_asyncio import AsyncIOMotorClient
+from dotenv import load_dotenv
 
-MONGO_URI = os.getenv("MONGO_URI", "mongodb://dbUser:<1234>@cluster1-shard-00-00.jb8e9.mongodb.net:27017,cluster1-shard-00-01.jb8e9.mongodb.net:27017,cluster1-shard-00-02.jb8e9.mongodb.net:27017/?ssl=true&replicaSet=atlas-ki02iw-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster1")
+load_dotenv()
+
+MONGO_URI = os.getenv("MONGO_URI")
 DB_NAME = "ml_database"
 
 client = AsyncIOMotorClient(MONGO_URI)
@@ -9,4 +12,3 @@ db = client[DB_NAME]
 
 student_collection = db.get_collection("students")
 question_collection = db.get_collection("questions")
-
